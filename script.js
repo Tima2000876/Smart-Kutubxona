c7076f58b24cea4dbb57297f44ee2d5043f72549
Tima2000876 <tima18273645@gmail.com>
2025-03-20 01:19:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-document.getElementById("book-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-
-    const book = { title, author };
-    let books = JSON.parse(localStorage.getItem("books")) || [];
-    books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
-
-    loadBooks();
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Ты нажал на кнопку!");
 });
-
-function loadBooks() {
-    let books = JSON.parse(localStorage.getItem("books")) || [];
-    const bookList = document.getElementById("book-list");
-    bookList.innerHTML = "";
-    books.forEach(book => {
-        const li = document.createElement("li");
-        li.textContent = ${book.title} - ${book.author};
-        bookList.appendChild(li);
-    });
-}
-
-loadBooks();
